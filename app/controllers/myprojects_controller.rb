3d72440ccfec91c00b6e4041737543dfb4fa6be7
y.kokubo <y.kokubo@otr-jp.com>
2011-09-13 15:36:28 +0900

    show user name on title

--- app/controllers/myprojects_controller.rb
+++ app/controllers/myprojects_controller.rb
@@ -2,8 +2,11 @@ class MyprojectsController < ApplicationController
   unloadable
   
   def index
+    @user = User.current
     @projects = Project.find(:all, :include => [:users, :members],
-                                   :conditions => ["#{Member.table_name}.user_id = ? AND #{Project.table_name}.status = ?", User.current.id, Project::STATUS_ACTIVE])
+                                   :conditions => ["#{Member.table_name}.user_id = ? AND #{Project.table_name}.status = ?", @user, Project::STATUS_ACTIVE])
+
+    @title = "#{l(:myprojects)} (#{@user.name})"
   end
 end
 
